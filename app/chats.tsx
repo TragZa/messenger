a43e297e77af6e1385d54042e727878f843963b1
TragZa <tragza3@gmail.com>
2024-01-08 22:16:32 +0530

    .

--- app/chats.tsx
+++ app/chats.tsx
@@ -19,7 +19,7 @@ export default function Chats() {
     const response = await fetch(`/api/user_conversations`, {
       method: "GET",
       headers: {
-        "Cache-Control": "no-cache",
+        "Cache-Control": "no-cache, no-store, max-age=0, must-revalidate",
       },
     });
 
